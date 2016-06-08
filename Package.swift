cf1be80ca17d2b6b68d5f8182db4f9bf138676d8
Kyle Jessup <badthing@gmail.com>
2016-06-08 16:25:44 -0400

    Tweaks for Linux build

--- Package.swift
+++ Package.swift
@@ -26,15 +26,15 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq.git", versions: Version(0,0,0)..<Version(10,0,0))
     ],
-    exclude: ["Sources/libpq"]
+    exclude: []
 )
 #else
 let package = Package(
     name: "PostgreSQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq.git", versions: Version(0,0,0)..<Version(10,0,0))
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq-linux.git", versions: Version(0,0,0)..<Version(10,0,0))
     ],
-    exclude: ["Sources/libpq"]
+    exclude: []
 )
 #endif
