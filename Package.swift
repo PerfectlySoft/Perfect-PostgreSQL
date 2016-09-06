6868013bbc4a9959e7c40d2c2e2c883f894a94b7
Kyle Jessup <badthing@gmail.com>
2016-09-06 16:18:43 -0400

    Pinned dep versions

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     name: "PostgreSQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq.git", versions: Version(0,0,0)..<Version(10,0,0))
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq.git", majorVersion: 2, minor: 0)
     ],
     exclude: []
 )
@@ -33,7 +33,7 @@ let package = Package(
     name: "PostgreSQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq-linux.git", versions: Version(0,0,0)..<Version(10,0,0))
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-libpq-linux.git", majorVersion: 2, minor: 0)
     ],
     exclude: []
 )
